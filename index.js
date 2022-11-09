d4430c6069583cf520dd588c033953bb54afa6c6
walter-bIack <leon.selyomin@gmail.com>
2022-11-09 12:35:40 -0500

    2
    2

--- index.js
+++ index.js
@@ -6,30 +6,12 @@ const DOMSelectors = {
 };
 
 function backgroundAndText(back, text) {
-  back.style.backgroundColor = "brown"; // element.style is used to make adjustments to the style of an element in js
-  text.innerHTML = "this is a big red box, now go look at console"; // .innerHTMl lets you edit an element, defined before the '.'. in this case it was "#text" being edited, defined by text.innerHTML. The desired element is selected through its assigned DOMSelector object.
+  back.style.backgroundColor = "red";
+  text.innerHTML = "this is a big blue box, now go look at console";
+  text.style.fontSize = "40px";
 }
-
-//   Event Listeners wait for a specified event to happen to an element, and then execute code when their conditions are met
+console.log(DOMSelectors.box);
 
 DOMSelectors.button.addEventListener("click", function () {
   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
 });
-
-// When you click the button element, the function 'changeBackground()' will run
-
-// eventlisteners can be applied to any element, and any amount of elements
-
-function changeLi() {
-  let i = 0;
-  DOMSelectors.points.forEach((el) => {
-    el.addEventListener("click", function () {
-      el.innerHTML = `${i}`; // you can use template literals in DOMSelectors as well!
-      i++;
-
-      console.log(i);
-    });
-  });
-}
-
-changeLi(); // will set the <li> value to 0 on first click, and the value will increase by 1 every time an <li> is clicked.
