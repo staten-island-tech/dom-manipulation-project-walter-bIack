5c5be676d02ac78f81f4b41067482c09c0ded6cc
walter-bIack <leon.selyomin@gmail.com>
2022-11-10 12:37:19 -0500

    2

--- index.js
+++ index.js
@@ -5,13 +5,68 @@ const DOMSelectors = {
   points: document.querySelectorAll(".point"),
 };
 
-function backgroundAndText(back, text) {
-  back.style.backgroundColor = "red";
-  text.innerHTML = "this is a big blue box, now go look at console";
-  text.style.fontSize = "40px";
-}
-console.log(DOMSelectors.box);
-
 DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+  const year = parseInt(document.getElementById("input").value);
 });
+
+if ((year = 1 && year <= 10)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Keith.</p> 
+`
+  );
+} else if ((year = 11 && year <= 20)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Ryan.</p> 
+`
+  );
+} else if ((year = 21 && year <= 30)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Mahamad.</p> 
+`
+  );
+} else if ((year = 31 && year <= 40)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Karen.</p> 
+`
+  );
+} else if ((year = 41 && year <= 50)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was donald.</p> 
+`
+  );
+} else if ((year = 51 && year <= 60)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was gertrud.</p> 
+`
+  );
+} else if ((year = 61 && year <= 70)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was John.</p> 
+`
+  );
+} else if ((year = 71 && year <= 80)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Jakie.</p> 
+`
+  );
+} else if ((year = 81 && year <= 90)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text">  the most popular name at the time was Qui.</p> 
+`
+  );
+} else if ((year = 91 && year <= 100)) {
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<p id="text"> the most popular name at the time was Sergay.</p> 
+`
+  );
+}
